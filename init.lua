5d2f5a4fbc8736a0413cb7e3e0f231cecdba0b94
Alex Wang <alexwng@amazon.com>
2024-04-15 20:19:40 -0700

    remove bad mapping

--- init.lua
+++ init.lua
@@ -6,10 +6,3 @@ vim.g.gruvbox_baby_function_style = "NONE"
 vim.g.gruvbox_baby_keyword_style = "NONE"
 
 vim.cmd.colorscheme("gruvbox-baby")
-
-vim.keymap.set("i", "<Tab>", 'copilot#Accept("\\<CR>")', { -- have to redefine this explicitly...
-  expr = true,
-  replace_keycodes = false,
-  noremap = true,
-  silent = true,
-})
