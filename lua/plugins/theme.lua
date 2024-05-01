4d61b53bb2d3cce5db8d39e2c35a008be2a34130
Alex Wang <alexwng@amazon.com>
2024-05-01 10:19:30 -0700

    light/dark switching commands

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,7 +1,25 @@
 return {
-  { "luisiacc/gruvbox-baby" },
+  {
+    "luisiacc/gruvbox-baby",
+    config = function()
+      vim.api.nvim_create_user_command("Baby", function()
+        vim.cmd("colorscheme gruvbox-baby")
+        vim.o.background = "dark"
+      end, {})
+    end,
+  },
   { "xiyaowong/transparent.nvim" },
   { "marko-cerovac/material.nvim" },
   { "scottmckendry/cyberdream.nvim" },
   { "rose-pine/neovim" },
+  { "sainnhe/gruvbox-material" },
+  {
+    "ellisonleao/gruvbox.nvim",
+    config = function()
+      vim.api.nvim_create_user_command("Light", function()
+        vim.cmd("colorscheme gruvbox")
+        vim.o.background = "light"
+      end, {})
+    end,
+  },
 }
