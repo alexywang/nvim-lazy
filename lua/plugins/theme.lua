db53644f5dcc8be98c4dd9876701462f2443e71c
Alex Wang <alex.yuanda.wang@gmail.com>
2024-05-12 21:48:57 -0700

    Themes

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,4 +1,16 @@
 return {
+  { "xiyaowong/transparent.nvim" },
+  { "marko-cerovac/material.nvim" },
+  { "sainnhe/gruvbox-material" },
+  {
+    "rose-pine/neovim",
+    config = function()
+      vim.api.nvim_create_user_command("Rose", function()
+        vim.cmd("colorscheme rose-pine")
+        vim.o.background = "dark"
+      end, {})
+    end,
+  },
   {
     "luisiacc/gruvbox-baby",
     config = function()
@@ -8,17 +20,46 @@ return {
       end, {})
     end,
   },
-  { "xiyaowong/transparent.nvim" },
-  { "marko-cerovac/material.nvim" },
-  { "scottmckendry/cyberdream.nvim" },
-  { "rose-pine/neovim" },
-  { "sainnhe/gruvbox-material" },
+  {
+    "scottmckendry/cyberdream.nvim",
+    config = function()
+      vim.api.nvim_create_user_command("Cyber", function()
+        vim.cmd("colorscheme cyberdream")
+        vim.o.background = "dark"
+      end, {})
+    end,
+  },
   {
     "ellisonleao/gruvbox.nvim",
     config = function()
       vim.api.nvim_create_user_command("Light", function()
         vim.cmd("colorscheme gruvbox")
         vim.o.background = "light"
+      end, {})
+    end,
+  },
+  {
+    "mcchrish/zenbones.nvim",
+    dependencies = { "rktjmp/lush.nvim" },
+    config = function()
+      vim.api.nvim_create_user_command("Zen", function()
+        vim.cmd("colorscheme zenbones")
+        vim.o.background = "dark"
+      end, {})
+
+      vim.api.nvim_create_user_command("ZenLight", function()
+        vim.o.termguicolors = true
+        vim.cmd("colorscheme zenbones")
+        vim.o.background = "light"
+      end, {})
+    end,
+  },
+  {
+    "savq/melange-nvim",
+    config = function()
+      vim.api.nvim_create_user_command("Melange", function()
+        vim.cmd("colorscheme melange")
+        vim.o.background = "dark"
       end, {})
     end,
   },
