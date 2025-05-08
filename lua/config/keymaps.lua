c2b8b3be88732349c834d63a526e8ebdb456e850
Alex Wang <alexwng@amazon.com>
2025-05-08 11:07:36 -0700

    file nav keymappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -31,3 +31,38 @@ vim.keymap.set("n", "<leader>p", ":CopyRelPath<CR>", { noremap = true, silent = 
 
 --show diagnostic
 vim.keymap.set("n", "<leader>dd", "<cmd> lua vim.diagnostic.open_float() <CR>", { noremap = true, silent = true })
+
+-- Yank filename under cursor using the same logic as 'gf'
+vim.keymap.set("n", "<leader>yf", function()
+  local filename = vim.fn.expand("<cfile>") -- gf uses this too
+  if filename ~= "" then
+    vim.fn.setreg("+", filename) -- system clipboard
+    print("Yanked to clipboard: " .. filename)
+  else
+    print("No valid filename under cursor")
+  end
+end, { desc = "Yank filename under cursor" })
+
+vim.keymap.set("n", "<leader>of", function()
+  local filename = vim.fn.expand("<cfile>")
+
+  if vim.fn.filereadable(filename) ~= 1 then
+    print("Not a valid file: " .. filename)
+    return
+  end
+
+  -- Search for a non-terminal window
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    local buf = vim.api.nvim_win_get_buf(win)
+    local buftype = vim.api.nvim_buf_get_option(buf, "buftype")
+    if buftype == "" then -- Normal buffer
+      -- Focus that window and open the file there
+      vim.api.nvim_set_current_win(win)
+      vim.cmd("edit " .. vim.fn.fnameescape(filename))
+      return
+    end
+  end
+
+  -- Fallback: open in a vsplit if no other window is available
+  vim.cmd("vsplit " .. vim.fn.fnameescape(filename))
+end, { desc = "Open file under cursor in a normal buffer" })
